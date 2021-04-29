f259608b41f13c6f09250cd284b9b8c5da466a7c
Максим Загорский <you@example.com>
2021-04-29 15:33:50 +0300

    Create Card class and added methot initialize with variable of suit, number, value

--- card.rb
+++ card.rb
@@ -1,1 +1,10 @@
+# Класс карты с мастью, номером, значением
+class Card
+  attr_reader :suit, :number, :value
 
+  def initialize(suit, number, value)
+    @suit = suit
+    @number = number
+    @value = value
+  end
+end
