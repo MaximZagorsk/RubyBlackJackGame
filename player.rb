f8102e00489be8839ec931d2b2ccd220691b0bf1
Максим Загорский <you@example.com>
2021-04-29 15:28:30 +0300

    Add variable @hand into Diller and Player classes

--- player.rb
+++ player.rb
@@ -1,6 +1,11 @@
 # Класс игрока
 class Player
+  # attr_accessor для чтения и записи карт
+  attr_accessor :hand
+
   def initialize(name)
     @name = name
+    # Переменная руки
+    @hand = []
   end
 end
