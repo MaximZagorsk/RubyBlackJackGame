106db89963abdb308aaea3385bea377d612f6a9c
Максим Загорский <you@example.com>
2021-04-29 22:13:24 +0300

    Method give_a_card into Deck class added and methods create_table and start_game added into Game class

--- game.rb
+++ game.rb
@@ -1,1 +1,21 @@
+# Класс игры
+class Game
+  def initialize
+    @table = []
+  end
 
+  # Метод создания стола
+  def create_table(player, diller, deck)
+    @table.push(player)
+    @table.push(diller)
+    @table.push(deck)
+  end
+
+  # Метод для начала партии, который кладет игрокам в руку по две карты
+  def start_game
+    2.times do
+      @table[2].give_a_card(@table[0])
+      @table[2].give_a_card(@table[1])
+    end
+  end
+end
