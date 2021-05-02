3333763133dcb8f9bdd1f85cf4cbffbedbeed943
Максим Загорский <you@example.com>
2021-05-02 20:57:40 +0300

    New method score in Player class created

--- game.rb
+++ game.rb
@@ -2,20 +2,29 @@
 class Game
   def initialize
     @table = []
+    @bank = 0
+    @player = nil
+    @diller = nil
+    @deck = nil
   end
 
   # Метод создания стола
   def create_table(player, diller, deck)
-    @table.push(player)
-    @table.push(diller)
-    @table.push(deck)
+    @player = player
+    @diller = diller
+    @deck = deck
   end
 
   # Метод для начала партии, который кладет игрокам в руку по две карты
   def start_game
+    @bank += 20
+    @player.cash -= 10
+    @player.cash -= 10
+    @bank += 20
     2.times do
-      @table[2].give_a_card(@table[0])
-      @table[2].give_a_card(@table[1])
+      @deck.give_a_card(@player)
+      @deck.give_a_card(@diller)
     end
   end
+
 end
