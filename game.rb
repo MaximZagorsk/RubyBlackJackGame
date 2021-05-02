ba2ec8d71551fe0da0920290f0bbd230b7af4bc3
Максим Загорский <you@example.com>
2021-05-02 21:50:35 +0300

    Parent class Player created to remove unnecessary code. Created method check_winner

--- game.rb
+++ game.rb
@@ -27,4 +27,26 @@ class Game
     end
   end
 
+  def check_winner
+    if @player.score > @diller.score
+      @player.cash += @bank
+      @bank -= @bank
+      @player.name
+    elsif @player.score < @diller.score
+      @diller.cash += @bank
+      @bank -= @bank
+      @diller.name
+    elsif @player.score == @diller.score
+      @player.cash += 10
+      @diller.cash += 10
+      @bank -= @bank
+      "Ничья"
+    end
+  end
+
+  def end_round
+    if (@player.hand.length == 3) and (@diller.hand.length == 3)
+      true
+    end
+  end
 end
