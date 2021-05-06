277fa59a7e02535a4f03fc3cf995c43a104ba349
Максим Загорский <you@example.com>
2021-05-06 13:48:10 +0300

    Method score replaced from Player class into Hand class

--- hand.rb
+++ hand.rb
@@ -1,2 +1,30 @@
 class Hand
+  def initialize
+    @cards = []
+  end
+
+  def score
+    @score = 0
+    card_a ||= 0
+    @hand.each do |card|
+      if card.number == 'A'
+        card_a += 1
+      else
+        @score += card.value
+      end
+    end
+    case card_a
+    when 1
+      @score += if @score + 11 > 21
+                  1
+                else
+                  11
+                end
+    when 2
+      @score += 2
+    when 3
+      @score = 13
+    end
+    @score
+  end
 end
--- player.rb
+++ player.rb
@@ -6,38 +6,12 @@ class Player
   def initialize(name)
     @name = name
     # Переменная руки
-    @hand = []
+    @hand = Hand.new
     @cash = 100
   end
 
   # Метод, который кладет карту в руку
   def take_a_card(card)
     @hand.push(card)
-  end
-
-  # Метод для подсчета очков
-  def score
-    @score = 0
-    card_a ||= 0
-    @hand.each do |card|
-      if card.number == 'A'
-        card_a += 1
-      else
-        @score += card.value
-      end
-    end
-    case card_a
-    when 1
-      @score += if @score + 11 > 21
-                  1
-                else
-                  11
-                end
-    when 2
-      @score += 2
-    when 3
-      @score = 13
-    end
-    @score
   end
 end
